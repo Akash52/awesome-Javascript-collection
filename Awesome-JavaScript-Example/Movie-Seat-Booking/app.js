52478eb2b71a43696b393cb18eac5ea0bbaa503d
Akash52 <ac8572611@gmail.com>
2020-11-06 13:24:48 +0530

    initial commit

--- Awesome-JavaScript-Example/Movie-Seat-Booking/app.js
+++ Awesome-JavaScript-Example/Movie-Seat-Booking/app.js
@@ -1,11 +1,4 @@
+const container = document.querySelector(".container");
 
-let name="Coding.7.7.7";
-let age=20;
-const hello=`My Name is ${name} and i am ${age}`;
-console.log(hello);
-const s='Hello World';
-console.log(s.length);
-console.log(s.toLowerCase());
-console.log(s.toUpperCase());
-console.log(s.substring(0,5).toUpperCase());
-console.log(s.split(''));
+const seats = document.querySelectorAll(".row.seat");
+console.log(seats);
