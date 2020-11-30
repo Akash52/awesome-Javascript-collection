0ed38cbf5888c94ad72af7e087753fb13907df7e
Akash52 <ac8572611@gmail.com>
2020-11-30 22:47:24 +0530

    Design improve

--- assets/js/profileAdd.js
+++ assets/js/profileAdd.js
@@ -1,8 +1,8 @@
 $.getJSON(
-  "https://raw.githubusercontent.com/Akash52/JS-Project-Display/master/data.json",
+  'https://raw.githubusercontent.com/Akash52/JS-Project-Display/master/data.json',
   (data) => {
     // console.log(data); // this will show the info it in firebug console
-    let profileKeys = ["handle", "image_link", "message", "message1"];
+    let profileKeys = ['handle', 'image_link', 'message', 'message1'];
     /**
      * Check if a profile has handle image_link and message properties
      */
@@ -23,26 +23,23 @@ $.getJSON(
     // get only unique and valid profiles
     let profiles = getUniqueProfiles(data.profiles.filter(isProfileValid));
 
-    let cardParent = document.getElementById("profile-cards");
+    let cardParent = document.getElementById('profile-cards');
     for (let index = 0; index < profiles.length; index += 1) {
-      let card = document.createElement("div");
+      let card = document.createElement('div');
 
       let = profile = profiles[index];
       card.innerHTML = `
-                    <div class="px-6 pt-6">
+                <div class="px-6 pt-4">
                     <div class="pt-6 text-center text-lg">
-                    
-                    <span>${profile.handle}</span>
-      
+                        <span></i>${profile.handle}</span>
                         <img alt="..." src='${profile.image_link}' class="shadow-lg rounded max-w-full mx-auto" style="max-width: 300px;" />
                         <div class="pt-6 text-center">
-                
                             <span><i class="fas fa-eye"></i><a href="${profile.message}"  target="_blank" class="no-underline hover:underline text-blue-500 text-lg"> Live Demo</a></span>
                             <span> <i class="fab fa-github"></i><a href="${profile.message1}" target="_blank" class="no-underline hover:underline text-orange-500 text-lg"> Repository</a></span>
                         </div>
                     </div>  
                 </div>
-            </div>
+       
 
 
         `;
