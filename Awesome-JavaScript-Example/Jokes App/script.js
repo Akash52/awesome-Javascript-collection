4220296f578177b4143630c56a648f2fbb3b2501
Akash52 <ac8572611@gmail.com>
2020-12-17 19:48:06 +0530

    Jokes Crack Added

---
+++ Awesome-JavaScript-Example/Jokes App/script.js
@@ -0,0 +1,20 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  fetch('https://icanhazdadjoke.com', config)
+    .then((res) => res.json())
+    .then((data) => {
+      jokeEl.innerHTML = data.joke
+    })
+}
