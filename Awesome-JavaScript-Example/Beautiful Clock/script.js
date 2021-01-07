95283730649f97fdef966290ee362d6752b3c530
Akash52 <ac8572611@gmail.com>
2021-01-07 13:46:41 +0530

    initial setup clock

---
+++ Awesome-JavaScript-Example/Beautiful Clock/script.js
@@ -0,0 +1,41 @@
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const toggle = document.querySelector('.toggle')
+
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+]
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+]
+
+toggle.addEventListener('click', (e) => {
+  const html = document.querySelector('html')
+  if (html.classList.contains('dark')) {
+    html.classList.remove('dark')
+    e.target.innerHTML = 'Dark mode'
+  } else {
+    html.classList.add('dark')
+    e.target.innerHTML = 'Light mode'
+  }
+})
