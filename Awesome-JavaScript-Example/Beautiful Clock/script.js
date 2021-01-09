f7fa366bd197c6cc3c1e07f224086cc293dc2109
Akash52 <ac8572611@gmail.com>
2021-01-09 11:12:02 +0530

    Project Updated

--- Awesome-JavaScript-Example/Beautiful Clock/script.js
+++ Awesome-JavaScript-Example/Beautiful Clock/script.js
@@ -39,3 +39,46 @@ toggle.addEventListener('click', (e) => {
     e.target.innerHTML = 'Light mode'
   }
 })
+
+function setTime() {
+  const time = new Date()
+  const month = time.getMonth()
+  const day = time.getDay()
+  const hours = time.getHours()
+  const hoursForClock = hours % 12
+  const minutes = time.getMinutes()
+  const seconds = time.getSeconds()
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursForClock,
+    0,
+    11,
+    0,
+    360
+  )}deg)`
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    59,
+    0,
+    360
+  )}deg)`
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    59,
+    0,
+    360
+  )}deg)`
+
+  timeEl.innerHTML = `${hoursForClock}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm}`
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
+
+setTime()
