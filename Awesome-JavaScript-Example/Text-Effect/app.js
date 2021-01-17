4f1856f7172f3b2a6082ec9793392f4e8c13fda0
tejas <tejas1188245@gmail.com>
2021-01-17 20:39:08 +0530

    data json

---
+++ Awesome-JavaScript-Example/Text-Effect/app.js
@@ -0,0 +1,25 @@
+const text = document.querySelector(".fancy");
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent= "";
+for(let i=0; i < splitText.length; i++)
+{
+    text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+let char = 0;
+let timer =setInterval(onTick, 50);
+
+function onTick(){
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++
+if(char === splitText.length){
+    complete();
+    return;
+    } 
+}
+
+function complete(){
+    clearInterval(timer);
+    time = null;
+}
