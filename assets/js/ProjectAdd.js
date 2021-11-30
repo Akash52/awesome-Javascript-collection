30e07004ad32d38b49f745729950c455a23a9325
Akash52 <ac8572611@gmail.com>
2021-11-30 23:04:22 +0530

    loading added

--- assets/js/ProjectAdd.js
+++ assets/js/ProjectAdd.js
@@ -1,30 +1,35 @@
-$.getJSON(
-  'https://raw.githubusercontent.com/Akash52/JS-Project-Display/master/data.json',
-  (data) => {
-    let profileKeys = ['handle', 'image_link', 'message', 'message1']
+const loading = document.getElementById('loading')
+jQuery(function ($) {
+  $(document).ajaxSend(function () {
+    $('#loading').fadeIn(300)
+  })
+  $.getJSON(
+    'https://raw.githubusercontent.com/Akash52/JS-Project-Display/master/data.json',
+    (data) => {
+      let profileKeys = ['handle', 'image_link', 'message', 'message1']
 
-    let isProfileValid = (profile) => profileKeys.every((k) => k in profile)
-    let getUniqueProfiles = (profiles) =>
-      Array.from(new Set(profiles.map((p) => p.handle))).map((id) => {
-        let profile = {}
-        profileKeys.forEach((k) => {
-          profile[k] = profiles.find((p) => p.handle === id)[k]
+      let isProfileValid = (profile) => profileKeys.every((k) => k in profile)
+      let getUniqueProfiles = (profiles) =>
+        Array.from(new Set(profiles.map((p) => p.handle))).map((id) => {
+          let profile = {}
+          profileKeys.forEach((k) => {
+            profile[k] = profiles.find((p) => p.handle === id)[k]
+          })
+          return profile
         })
-        return profile
-      })
-    // get only unique and valid profiles
-    let profiles = getUniqueProfiles(data.profiles.filter(isProfileValid))
+      // get only unique and valid profiles
+      let profiles = getUniqueProfiles(data.profiles.filter(isProfileValid))
 
-    let cardParent = document.getElementById('profile-cards')
-    for (let index = 0; index < profiles.length; index += 1) {
-      let card = document.createElement('div')
-      let = profile = profiles[index]
-      card.innerHTML = `
+      let cardParent = document.getElementById('profile-cards')
+      for (let index = 0; index < profiles.length; index += 1) {
+        let card = document.createElement('div')
+        let = profile = profiles[index]
+        card.innerHTML = `
       <div className="max-w-md py-4 px-8  shadow-lg rounded-lg my-2">
-                    <div class=" pt-6 text-center text-xl">
+                    <div class="pt-6 text-center text-xl">
                         <span class="text-xl hover:text-orange-500">${profile.handle}</span>
                         <div class="w-6/12 sm:w-4/12 px-4">
-                        <img alt="..." src='${profile.image_link}' class="shadow-lg rounded max-w-full  h-auto " style="max-width:250px;max-height:400px" />
+                        <img alt="..." src='${profile.image_link}' class="shadow-lg rounded max-w-full  h-auto" style="max-width:250px;max-height:400px" />
                         </div>
                         <div class="p-5 text-center">
                             <span><i class="fas fa-globe"></i><a href="${profile.message}"  target="_blank" class="no-underline  text-blue-500 text-xl"> Live Demo</a></span>
@@ -33,7 +38,12 @@ $.getJSON(
                     </div>  
                   </div>     
         `
-      cardParent.appendChild(card)
+        cardParent.appendChild(card)
+      }
     }
-  }
-)
+  ).done(function () {
+    setTimeout(function () {
+      $('#loading').fadeOut(300)
+    }, 3000)
+  })
+})
